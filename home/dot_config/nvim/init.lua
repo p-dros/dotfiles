9e91acedc1eb415ac08329236e3d5f3e97b6f342
pdros <piotr.dros01@gmail.com>
2026-06-26 19:23:53 +0200

    refactor

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,18 +1,19 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-vim.o.number = true
+
+vim.opt.breakindent = true
+vim.opt.cursorline = true
+vim.opt.ignorecase = true
+vim.opt.number = true
 vim.opt.relativenumber = true
-vim.o.showmode = false
+vim.opt.showmode = false
+vim.opt.signcolumn = 'yes'
+vim.opt.smartcase = true
+vim.opt.undofile = true
+vim.opt.updatetime = 250
+
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.schedule(function()
     vim.opt.clipboard = 'unnamedplus'
 end)
-
-vim.o.breakindent = true
-vim.o.undofile = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.signcolumn = 'yes'
-vim.o.updatetime = 250
-vim.o.cursorline = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
